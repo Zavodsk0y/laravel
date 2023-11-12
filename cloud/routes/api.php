c93cb2a9f03349d47b4cebb0b9bf17cb92b9a44d
Zavodsk0y <zimniyvolk5@gmail.com>
2023-11-12 21:34:20 +0700

    Меняем дефолтный адрес, группируем роуты

--- cloud/routes/api.php
+++ cloud/routes/api.php
@@ -17,7 +17,6 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::post('/registration', [ApiAuthController::class, 'register']);
-
 Route::post('/authorization', [ApiAuthController::class, 'authorization']);
 
 Route::middleware(['auth:sanctum'])->group(function () {
